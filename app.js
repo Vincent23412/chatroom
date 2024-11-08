4f9dc2d1a0e1fb7ed066d50994658462e001de6d
Vincent23412 <87458133+Vincent23412@users.noreply.github.com>
2024-11-09 00:36:34 +0800

    1. add getAllMessage

--- app.js
+++ app.js
@@ -50,10 +50,40 @@ app.get('/protect', auth.authenticateToken, (req, res) => {
     res.json({message: 'welcome'}); 
 })
 
-
 setupWebSocket(server);
 
+app.get('/getAllMessage', (req, res) => {
+        sendAllMessage(pool, res)
+    }
+)
 
 server.listen(process.env.PORT , '0.0.0.0', () => {
     console.log("Server is running on port", process.env.PORT); 
 });
+
+
+
+
+function sendAllMessage(pool, res)
+{
+    pool.query('SELECT * FROM chat', (err, results) => {
+        if (err) {
+            console.error('Error executing query:', err.stack); // 處理資料庫錯誤
+            ws.send(JSON.stringify({
+                status: 'error',
+                message: 'Failed to retrieve messages from the database'
+            }));
+        } else {
+            // 查詢成功，將結果發送給所有已連接的用戶
+            const messages = results.rows; // 假設結果存儲在 rows 中
+            const messageData = {
+                status: 'success',
+                context: 'chatHistory',
+                messages
+            };
+    
+            // 廣播給所有已連接的 WebSocket 用戶
+            res.send(JSON.stringify(messageData));
+        }
+    });
+}
